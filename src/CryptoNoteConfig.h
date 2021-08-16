dc1f4740ecf7b78eb611ddebcdf0308ec3596a65
Camellia73 <blackrosecoin@tutanota.com>
2021-08-16 19:03:01 +0600

    Update seed nodes

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -135,7 +135,7 @@ const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT                    = 60 * 2 * 1000; //
 const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          // 5 seconds
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 
-const char* const SEED_NODES[] = { "communitypool.org:28688", "xvv.communitypool.org:28688", "192.95.20.54:28688", "144.217.29.36:28688", "explorer.xcoin.ninja:28688", "144.217.29.34:28688", "144.217.30.38:28688" };
+const char* const SEED_NODES[] = { "communitypool.org:28688", "explorer.xcoin.ninja:28688", "54.39.195.212:28688" };
 
 
 struct CheckpointData {
